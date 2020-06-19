2c0aced449802ea71cecc0b0c5c12e7101b97398
YuSeongChoi <marine8743@naver.com>
2020-06-19 18:02:42 +0900

    ViewController 미션

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello", message: "My First App!", preferredStyle:.alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func challenge(_ sender: Any) {
+        let alert = UIAlertController(title: "SoongSil Univ.", message: "Software 20150285", preferredStyle:.alert)
+        let action = UIAlertAction(title: "BYE", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
 
+    }
 }
 
