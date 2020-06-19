6be295e3f0940c8b8327bf89ed8fd945af3da210
YuSeongChoi <marine8743@naver.com>
2020-06-19 17:30:40 +0900

    Album App 1

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by Mac on 2020/06/19.
+//  Copyright © 2020 com.yuseong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
