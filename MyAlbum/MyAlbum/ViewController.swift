dff0253bea44d13e651732197b7cff50cf6be5bb
YuSeongChoi <marine8743@naver.com>
2020-06-19 19:01:48 +0900

    outlet 연결해보기

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -9,25 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue = 0
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        priceLabel.text = "$ \(currentValue)"
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello", message: "My First App!", preferredStyle:.alert)
+        let message = "가격은 \(currentValue) 입니다"
+        let alert = UIAlertController(title: "Price", message: message, preferredStyle:.alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        priceLabel.text = "$ \(currentValue)"
     }
     
-    @IBAction func challenge(_ sender: Any) {
-        let alert = UIAlertController(title: "SoongSil Univ.", message: "Software 20150285", preferredStyle:.alert)
-        let action = UIAlertAction(title: "BYE", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-
-    }
 }
 
