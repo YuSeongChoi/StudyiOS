3b6a462dd81d1cc102b54f85ee71bc1539f5b4b5
YuSeongChoi <marine8743@naver.com>
2020-06-20 18:58:31 +0900

    Closure 기본 개념

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -15,17 +15,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        priceLabel.text = "$ \(currentValue)"
+        refresh()
     }
 
     @IBAction func showAlert(_ sender: Any) {
         let message = "가격은 \(currentValue) 입니다"
         let alert = UIAlertController(title: "Price", message: message, preferredStyle:.alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {  action in
+            self.refresh()
+        })
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
+    }
+    
+    func refresh() {
         let randomPrice = arc4random_uniform(10000) + 1
         currentValue = Int(randomPrice)
         priceLabel.text = "$ \(currentValue)"
