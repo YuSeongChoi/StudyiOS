915897a952ab75acdb3e534b8ca2583f82499df8
YuSeongChoi <marine8743@naver.com>
2020-06-26 15:49:14 +0900

    Tuple 기본 사용법

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,3 +4,21 @@ import UIKit
 
 let value = arc4random_uniform(100)
 print("--->\(value)")
+
+
+// --- Tuple
+
+let coordinates = (4,6)
+
+let x = coordinates.0
+let y = coordinates.1
+
+let coordinatesNamed = (x: 2, y: 3)
+
+let x2 = coordinatesNamed.x
+let y2 = coordinatesNamed.y
+
+let (x3, y3) = coordinatesNamed
+
+x3
+y3
