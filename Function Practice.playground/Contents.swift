a4f9186fcc7ffa488a66cedb1d1078e0948b1b5b
YuSeongChoi <marine8743@naver.com>
2020-07-02 01:35:24 +0900

    고급기능

--- Function Practice.playground/Contents.swift
+++ Function Practice.playground/Contents.swift
@@ -21,3 +21,54 @@ func printFullName3(first: String, last: String) -> String {
 }
 let fullname = printFullName3(first: "유성", last: "최")
 print(fullname)
+
+//func functionName(externalName param: ParamType) -> ReturnType {
+//    //......
+//    return returnValue
+//}
+
+// overloading
+func printTotalPrice(price: Int, count: Int) {
+    print("Total Price : \(price * count)")
+}
+
+func printTotalPrice(price: Double, count: Double) {
+    print("Total Price: \(price * count)")
+}
+
+func printTotalPrice(가격: Double, 개수: Double) {
+    print("Total Price: \(가격 * 개수)")
+}
+
+// In-out parameter
+
+var value = 3
+func incrementAndPrint(_ value: inout Int) {
+    value += 1
+    print(value)
+}
+
+incrementAndPrint(&value)
+
+// ---- Function as a param
+
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func subtract(_ a: Int, _ b: Int) -> Int {
+    return a - b
+}
+
+var function = add
+function(4,2)
+function = subtract
+function(4,2)
+
+func printResult(_ function: (Int, Int) -> Int, _ a:Int, _ b: Int) {
+    let result = function(a,b)
+    print(result)
+}
+
+printResult(add, 10, 5)
+printResult(subtract, 10, 5)
