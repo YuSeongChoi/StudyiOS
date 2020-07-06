b2de14ff27edfdcfd24e2970c29c5a08aa139c0f
YuSeongChoi <marine8743@naver.com>
2020-07-06 17:10:57 +0900

    스위프트 Optional 개념과 간단한 예제

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var carName: String?
+carName = nil
+carName = "Maserati"
+
+// 아주 간단한 과제
+// 1. 최애 영화 배우의 이름을 담는 변수 작성 (타입 String?)
+// 2. let num = Int("10") -> 타입?
+var favoriteMovieStar: String? = nil
+let num = Int("10")
+
+// Int의 소괄호 안이 변환이 될 수도 있고 안될 수도 있기 때문에 Int?형으로 캐스팅된 것.
+
+
