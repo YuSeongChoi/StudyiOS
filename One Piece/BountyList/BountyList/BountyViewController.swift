42e31fc079dd8166a0b525902180e3483a2d1001
YuSeongChoi <marine8743@naver.com>
2020-07-30 15:28:37 +0900

    MVVM Model 리팩토링

--- One Piece/BountyList/BountyList/BountyViewController.swift
+++ One Piece/BountyList/BountyList/BountyViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
-    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
+    
+    // MVVM Model
+    
+    // Model
+    // - BountyInfo
+    // -> BountyInfo 만들자
+    
+    // View
+    // - ListCell
+    // -> ListCell에 필요한 정보를 ViewModel한테 받아야한다.
+    // -> ListCell은 ViewModel에게서 받은 정보로 뷰 업데이트
+    
+    // ViewModel
+    // - BountyViewModel을 만든다.
+    // -> BountyViewModel을 만들고, 뷰 레이어에서 필요한 메소드 만들기
+    // -> 모델 가지고 있기, BountyInfo 들
+    
+    let bountyInfoList: [BountyInfo] =  [
+        BountyInfo(name: "brook", bounty: 33000000),
+        BountyInfo(name: "chopper", bounty: 50),
+        BountyInfo(name: "franky", bounty: 44000000),
+        BountyInfo(name: "luffy", bounty: 300000000),
+        BountyInfo(name: "nami", bounty: 16000000),
+        BountyInfo(name: "robin", bounty: 80000000),
+        BountyInfo(name: "sanji", bounty: 77000000),
+        BountyInfo(name: "zoro", bounty: 120000000)
+    ]
+    
+//    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+//    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //DetailViewController 데이터 넘겨주기
         if segue.identifier == "showDetail" {
             let vc = segue.destination as? DetailViewController
             if let index = sender as? Int {
-                vc?.name = nameList[index]
-                vc?.bounty = bountyList[index]
+                let bountyInfo = bountyInfoList[index]
+//                vc?.name = bountyInfo.name
+//                vc?.bounty = bountyInfo.bounty
+                vc?.bountyInfo = bountyInfo
             }
         }
     }
@@ -30,7 +60,8 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         
         // UITableViewDataSource
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return bountyList.count
+//            return bountyList.count
+            return bountyInfoList.count
         }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -38,21 +69,17 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
                 return UITableViewCell()
             }
             
-            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
-            cell.imgView.image = img
-            cell.nameLabel.text = nameList[indexPath.row]
-            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
-            return cell
+//            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+//            cell.imgView.image = img
+//            cell.nameLabel.text = nameList[indexPath.row]
+//            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
             
-    //        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
-    //            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
-    //            cell.imgView.image = img
-    //            cell.nameLabel.text = nameList[indexPath.row]
-    //            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
-    //            return cell
-    //        } else {
-    //            return UITableViewCell()
-    //        }
+            let bountyInfo = bountyInfoList[indexPath.row]
+            cell.imgView.image = bountyInfo.image
+            cell.nameLabel.text = bountyInfo.name
+            cell.bountyLabel.text = "\(bountyInfo.bounty)"
+
+            return cell
         }
         
         // UITableViewDelegate
@@ -67,3 +94,17 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
         @IBOutlet weak var nameLabel: UILabel!
         @IBOutlet weak var bountyLabel: UILabel!
     }
+
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name: String, bounty: Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
