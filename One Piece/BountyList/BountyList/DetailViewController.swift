1d3b0509cc7ac07ab4d861e91d255e1cbd576306
YuSeongChoi <marine8743@naver.com>
2020-07-30 13:14:31 +0900

    모달로 띄우기

---
+++ One Piece/BountyList/BountyList/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  BountyList
+//
+//  Created by Mac on 2020/07/30.
+//  Copyright © 2020 com.yuseong. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
