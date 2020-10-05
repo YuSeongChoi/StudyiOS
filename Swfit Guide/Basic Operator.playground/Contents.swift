cd32391bc9332e724fa0277063de4038aa8a3cdc
YuSeongChoi <marine8743@naver.com>
2020-10-05 20:38:46 +0900

    Basic Operator 2

--- Swfit Guide/Basic Operator.playground/Contents.swift
+++ Swfit Guide/Basic Operator.playground/Contents.swift
@@ -47,3 +47,31 @@ var userDefineColor: String?
 var colorName = userDefineColor ?? defaultColor
 userDefineColor = "green"
 colorName = userDefineColor ?? defaultColor
+
+// 범위 연산자
+
+// 1. 닫힌 범위 연산자
+for index in 1...5 {
+    print("\(index) times 5 is \(5 * index)")
+}
+print("")
+// 2. 반 닫힌 범위 연산자
+let names = ["Nuguri", "Canyon","Showmaker","Ghost","Beryl"]
+let count = names.count
+for i in 0..<count {
+    print("Damwon Gaming Member \(i+1) is \(names[i])")
+}
+print("")
+// 3. 단방향 범위
+// [a..] , [..a]의 형태로 범위의 시작 혹은 끝만 지정해 사용하는 범위 연산자
+// 지정한 시작 값 혹은 끝 값도 범위에 포함
+for name in names[2...] {
+    print(name)
+}
+print("")
+for name in names[...2] {
+    print(name)
+}
+// [..<2] 도 사용 가능 , 이 때는 범위에 포함되지 않는다.
+
+
