b6bf254f4c15c00c02d422522079dde4a82ec93d
YuSeongChoi <marine8743@naver.com>
2020-10-08 17:21:51 +0900

    문자열과 문자 1

---
+++ Swfit Guide/Strings and Charaters.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// The Swift Language Guide
+// 2. 문자열과 문자
+
+for character in "Dog!" {
+    print(character)
+}
+
+let catCharacters: [Character] = ["C","a","t","!"]
+let catString = String(catCharacters)
+print(catString)
