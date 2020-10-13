cf0e6bcbf2418dcc05b9e0a2de8807b382732602
YuSeongChoi <marine8743@naver.com>
2020-10-13 14:43:05 +0900

    SearchAPI 구현 1

--- MyNetflix/MyNetflix/SearchViewController.swift
+++ MyNetflix/MyNetflix/SearchViewController.swift
@@ -31,6 +31,61 @@ extension SearchViewController: UISearchBarDelegate {
         // 검색어가 있는지
         guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else {return}
         // 네트워킹을 통한 검색
+        // - 목표 : 서치텀을 가지고 네트워크를 이용해 영화 검색
+        // - 검색 API가 필요
+        // - 결과를 받아올 모델 Movie, Response
+        // - 결과를 받아와서, CollectionView로 표현해주자
+        
+        SearchAPI.search(searchTerm) { movies in
+            // collectionView로 표현하기
+        }
         print("--> 검색어 \(searchTerm)")
     }
+}
+
+class SearchAPI {
+    static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        let session = URLSession(configuration: .default)
+        
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entitiyQuery = URLQueryItem(name: "entitiy", value: "movie")
+        let termQuery = URLQueryItem(name: "term", value: term)
+        urlComponents.queryItems?.append(mediaQuery)
+        urlComponents.queryItems?.append(entitiyQuery)
+        urlComponents.queryItems?.append(termQuery)
+        
+        let requestURL = urlComponents.url!
+        
+        let dataTask = session.dataTask(with: requestURL) { data, response, error in
+            let successRange = 200..<300
+            
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                  successRange.contains(statusCode) else {
+                    completion([])
+                    return
+            }
+            
+            guard let resultData = data else {
+                completion([])
+                return
+            }
+            
+            //data -> [Movie]로 파싱해줘야함
+            let string = String(data: resultData, encoding: .utf8)
+            print("---> result : \(string)")
+//            completion([Movie])
+            
+        }
+        dataTask.resume()
+    }
+}
+
+struct Response {
+    
+}
+
+struct Movie {
+    
 }
