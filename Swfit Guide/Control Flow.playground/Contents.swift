badef25616c0adc5e461024dcc0314c0836e49d8
YuSeongChoi <marine8743@naver.com>
2020-10-17 17:03:09 +0900

    조건적 구문

--- Swfit Guide/Control Flow.playground/Contents.swift
+++ Swfit Guide/Control Flow.playground/Contents.swift
@@ -37,3 +37,129 @@ for tickMark in stride(from: 0, to: 15, by: minuteInterval) {
     // render the tick mark every 5 minutes (0, 5, 10, 15 ... 45, 50, 55)
     print(tickMark)
 }
+
+// While 문 (While Loops)
+// 1. While
+// 일반적인 While 문
+
+// 2. Repeat-While
+// do-While과 같음
+
+// 조건문
+let temperatureInFahrenheit = 90
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+    print("It's not that cold. Wear a t-shirt.")
+}
+// Prints "It's really warm. Don't forget to wear sunscreen."
+
+// switch 문
+let someCharacter: Character = "z"
+switch someCharacter {
+case "a":
+    print("The first letter of the alphabet")
+case "z":
+    print("The last letter of the alphabet")
+default:
+    print("Some other character")
+}
+// Prints "The last letter of the alphabet"
+// swift에서는 break를 쓰지 않아도 됌
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+case "a", "A":
+    print("The letter A")
+default:
+    print("Not the letter A")
+}
+// Prints "The letter A"
+
+// 숫자의 특정 범위를 조건으로 사용 가능
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+// Prints "There are dozens of moons orbiting Saturn."
+
+// 튜플을 조건으로 사용 가능
+let somePoint = (1, 1)
+switch somePoint {
+case (0, 0):
+    print("\(somePoint) is at the origin")
+case (_, 0):
+    print("\(somePoint) is on the x-axis")
+case (0, _):
+    print("\(somePoint) is on the y-axis")
+case (-2...2, -2...2):
+    print("\(somePoint) is inside the box")
+default:
+    print("\(somePoint) is outside of the box")
+}
+// Prints "(1, 1) is inside the box"
+
+// 값 바인딩
+// 특정 x,y 값을 각각 다른 case에 정의하고 그 정의된 상수를 또 다른 case에서 사용 가능.
+// 이런 기법을 값-바인딩이라고 부른다.
+let anotherPoint = (2, 0)
+switch anotherPoint {
+case (let x, 0):
+    print("on the x-axis with an x value of \(x)")
+case (0, let y):
+    print("on the y-axis with a y value of \(y)")
+case let (x, y):
+    print("somewhere else at (\(x), \(y))")
+}
+// Prints "on the x-axis with an x value of 2"
+
+
+// where 문
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+// Prints "(1, -1) is on the line x == -y"
+
+// 혼합 케이스
+let someChar: Character = "e"
+switch someChar {
+case "a", "e", "i", "o", "u":
+    print("\(someChar) is a vowel")
+case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+     "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+    print("\(someChar) is a consonant")
+default:
+    print("\(someChar) is not a vowel or a consonant")
+}
+// Prints "e is a vowel"
+
+// 혼합 케이스에서도 값 바인딩 사용 가능
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("Not on an axis")
+}
+// Prints "On an axis, 9 from the origin"
