5dd414d19fab202bc9eeb5fc430bbbaeef51d688
YuSeongChoi <marine8743@naver.com>
2020-10-22 16:53:01 +0900

    Firebase101

---
+++ Firebase101/Firebase101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase101
+//
+//  Created by Mac on 2020/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Firebase101/Pods/Target Support Files/Pods-Firebase101-Firebase101UITests/Pods-Firebase101-Firebase101UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Firebase101_Firebase101UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Firebase101_Firebase101UITests
+@end
