246f7bb7ca95ca49d52103accec5802cc6cabf28
YuSeongChoi <marine8743@naver.com>
2020-11-11 16:34:21 +0900

    MyNetflix 앱 완성

--- MyNetflix/MyNetflix/HistoryViewController.swift
+++ MyNetflix/MyNetflix/HistoryViewController.swift
@@ -13,6 +13,7 @@ class HistoryViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     let db = Database.database().reference().child("searchHistory")
+    var searchTerms: [SearchTerm] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +23,36 @@ class HistoryViewController: UIViewController {
         super.viewWillAppear(animated)
         
         db.observeSingleEvent(of: .value) { (snapshot) in
-            print("---> snapshot : \(snapshot.value)")
+            guard let searchHistry = snapshot.value as? [String:Any] else { return }
+            let data = try! JSONSerialization.data(withJSONObject: Array(searchHistry.values), options: [])
+            let decoder = JSONDecoder()
+            let searchTerms = try! decoder.decode([SearchTerm].self, from: data)
+            self.searchTerms = searchTerms.sorted{ $0.timestamp > $1.timestamp }
+            self.tableView.reloadData()
+            print("---> snapshot : \(data), \(searchTerms)")
         }
     }
 }
+
+extension HistoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchTerms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath) as? HistoryCell else {
+            return UITableViewCell()
+        }
+        cell.searchTerm.text = searchTerms[indexPath.row].term
+        return cell
+    }
+}
+
 class HistoryCell: UITableViewCell {
     @IBOutlet weak var searchTerm: UILabel!
-        
+}
+
+struct SearchTerm: Codable {
+    let term: String
+    let timestamp: TimeInterval
 }
