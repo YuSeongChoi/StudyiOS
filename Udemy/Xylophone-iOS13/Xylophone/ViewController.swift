5ed6681f35919bcb54abe7223f6c742d5e23a3d7
YuSeongChoi <marine8743@naver.com>
2020-12-01 16:01:16 +0900

    실로폰 플젝 완료

--- Udemy/Xylophone-iOS13/Xylophone/ViewController.swift
+++ Udemy/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -10,11 +10,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        if let key = sender.currentTitle {
+            playSound(key)
+        }
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(_ key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
