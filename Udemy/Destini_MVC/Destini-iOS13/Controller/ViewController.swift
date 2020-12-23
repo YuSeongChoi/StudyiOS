7101be7fac03cbd1aac340ebc7e06105388e324b
YuSeongChoi <marine8743@naver.com>
2020-12-23 15:42:20 +0900

    구조 생성해보기

--- Udemy/Destini_MVC/Destini-iOS13/Controller/ViewController.swift
+++ Udemy/Destini_MVC/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+        Story(title: "You see a tiger", choice1: "Shout for help", choice2: "play dead"),
+        Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+    ]
+    
+    let story0 = "길에 갈림길이 보입니다."
+    let choice1 = "왼쪽으로 갑니다."
+    let choice2 = "오른쪽으로 갑니다."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
 }
 
