21d960367a08c1f6313757fdb6b83f27057ae831
YuSeongChoi <marine8743@naver.com>
2021-09-10 20:06:11 +0900

    ADD 명언 생성기 완성

---
+++ projects-30/QuotesGenerator/QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by Mac on 2021/09/10.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
--- projects-30/QuotesGenerator/QuotesGenerator/ViewController.swift
+++ projects-30/QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/*
+ Content Hugging Priorty
+ Content Compression Resistance Priorty
+ */
 
+class ViewController: UIViewController {
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let quotes = [
+        Quote(contents: "부진은 있어도 몰락은 없다.", name: "kkoma 김정균"),
+        Quote(contents: "똑바로 ! 짝! 뛰라고! 짝! 이 아이야! 짝!", name: "DS Khan"),
+        Quote(contents: "가지가지 나뭇가지", name: "T1 Faker"),
+        Quote(contents: "나라는 괴물..... 이게... 나야....", name: "쓰레기 클템"),
+        Quote(contents: "슉.. 슈슉... 슉.. 슈슈슉..슉..슈슈슈슉..슉..슈슉..", name: "돌하르방")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapQouteGeneratorButton(_ sender: Any) {
+        let random = Int(arc4random_uniform(5)) // 0~4 사이의 상수들
+        let quote = quotes[random]
+        
+        quoteLabel.text = quote.contents
+        nameLabel.text = quote.name
+    }
+    
 }
 
